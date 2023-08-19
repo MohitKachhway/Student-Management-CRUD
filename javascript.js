78766c9a2589ea5f3b1e8a06ce70fcf28a73f04e
MohitKachhway <mohitkachhway54@gmail.com>
2023-08-19 11:51:58 +0530

    submission

--- javascript.js
+++ javascript.js
@@ -57,16 +57,48 @@ const students = [
  function addRowNode(newObj){
 
     let row = document.createElement("tr");
+    row.id= newObj.ID;
     row.innerHTML = ` <th>${newObj.ID}</th>
                     <th>${newObj.name}</th>
                     <th>${newObj.email}</th>
                     <th>${newObj.age}</th>
                     <th>${newObj.grade}</th>
-                    <th>${newObj.degree}</th>`
+                    <th id="deg">${newObj.degree}<span class="fun-btn fun-btn-ed"><img src="./image/edit 1.png" alt="edit" id="edit"></span><span class="fun-btn fun-btn-del"><img src="./image/trash-2 1.png" alt="delete" id="delete"></span></th>`
 
     let table = document.getElementById("table");
     table.appendChild(row);
  };
-  
 
- 
+// // Now we delete row .....
+let deleteButtons = document.getElementsByClassName("fun-btn-del") ; // array of all span ele of delete button.
+for(let i=0;i<deleteButtons.length;++i){
+    deleteButtons[i].addEventListener("click",(e)=>{
+        let delRow = deleteButtons[i].parentNode.parentNode;
+        delRow.remove();
+    })
+}
+
+// let editButton = document.getElementsByClassName("fun-btn-ed");
+// for(let i=0; i<editButton.length;++i){
+
+//     editButton[i].addEventListener("click",(e)=>{
+//         let editRow = editButton[i].parentNode.parentNode; //<tr>
+//         let rowChild =editRow.children;
+//         // console.log(editRow);
+        
+//         for (let i=0; i<inputs.length;++i){
+//             inputs[i].value = rowChild[i].textContent;
+//         }
+//     // console.log(inputs[0].value="mohit")
+       
+//         console.log(c[1].innerHTML)
+//     })
+// }
+
+//-----------------------------filter apply-----------------------------------
+// let table = document.getElementById("table");
+// let rowObj = table.children[0].children[0]; 
+
+// for( let i=0; i<rowObj.length;++i){
+//     console.log(rowObj[i]);
+// }
